151c968c280b28c61afabc334c9c0be35f508ec3
Or <ohanoch@gmail.com>
2016-10-09 17:57:08 +0200

    face detection josh working

--- Or_excercises/ex1.py
+++ Or_excercises/ex1.py
@@ -11,3 +11,10 @@ cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.waitKey(0)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite('onion.png',img)
